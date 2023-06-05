58ee04b62c1900e1af2bc8a14267b363668f5876
Lázaro Alves <geo.eolo@gmail.com>
2023-06-04 22:14:20 -0300

    fix deploy

--- data/database.js
+++ data/database.js
@@ -1,27 +1,22 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient } = require("mongodb");
 
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri =
+  "mongodb+srv://lazaro:R48h5wFwgnpLQyk@cluster0.jrofzkp.mongodb.net/?retryWrites=true&w=majority";
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+const client = new MongoClient(uri);
+
+console.log("Trying to connect to db");
 
 async function run() {
   try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    await client.db();
+    await client.db("gha-database").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connect to MongoDB!");
   } finally {
     await client.close();
   }
