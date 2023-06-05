4478004b5914566270b9da136d77fd4cb05dc926
Lázaro Alves <geo.eolo@gmail.com>
2023-06-04 21:41:31 -0300

    add connection db

--- data/database.js
+++ data/database.js
@@ -1,4 +1,4 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
@@ -27,5 +27,3 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-export default database;
